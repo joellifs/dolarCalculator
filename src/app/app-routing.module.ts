2b94b34632e0eac57b503995286fa22c8c0cb6c9
Joel <joel@MacBook-Air-de-Joel.local>
2021-02-18 13:25:43 -0300

    Ruteo de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { HistoricoComponent } from './historico/historico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CalculadoraComponent,
+  },
+  {
+    path: 'historico',
+    component: HistoricoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { HistoricoComponent } from './historico/historico.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CalculadoraComponent, HistoricoComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
