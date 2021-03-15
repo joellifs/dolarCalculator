07374e9ffd4b398e67c6aae0e536a942db413b44
joaquin <joaquinlifs0206@gmail.com>
2021-03-15 15:13:23 -0300

    prueba llenado de tabla

--- src/app/historico/historico.component.ts
+++ src/app/historico/historico.component.ts
@@ -1,3 +1,4 @@
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './historico.component.html',
   styleUrls: ['./historico.component.css']
 })
+
 export class HistoricoComponent implements OnInit {
-dataSource= [];
+element: any = {
+  hora: "12:00",
+  operacion: "venta",
+  usd: "1",
+  ars: "1",
+} ;
+
+elemen2:any = {
+  hora: "13:00",
+  operacion: "venta",
+  usd: "2",
+  ars: "2",
+};
+  dataSource= [this.element, this.elemen2    ];
 displayedColumns=["hora", "operacion", "usd", "ars"];
   constructor() { }
 
